7e2f5bd9124a5f342cc80849919f1c3042a30eb8
jon geiger <git@geiger.to>
2024-07-06 18:05:32 -0400

    change installation procedure to a ruby script
    plus...
    
     - add autosaving
     - prompt for name + email (in git)
     - gnu-sed (for <Leader>sr in neovim)

---
+++ install.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+DIR = __dir__
+
+module CLI
+  def self.main
+    cmd = ARGV.first
+    case cmd
+    when "brew"
+      system "brew bundle" unless ARGV.include?('--skip-install')
+    when "apt"
+      # todo
+    else
+      puts "error: you must specify a target (brew|apt)..."
+      puts
+      puts "e.g. ./install.rb brew"
+      puts
+      exit 1
+    end
+
+    system "ansible-playbook ansible/#{cmd}.yml"
+  end
+end
+
+CLI.main
